ca21a626f313536186507f53516b9403d2ac5884
Masui Masanori <masanori840816@gmail.com>
2026-02-03 02:32:05 +0900

    check sign-in values

--- Assets/Scripts/SigninPage.cs
+++ Assets/Scripts/SigninPage.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class SigninPage : MonoBehaviour
 {
+    [SerializeField] private SaveDataManager saveDataManager;
+    [SerializeField] private TMPro.TMP_InputField mailAddressInput;
+    [SerializeField] private TMPro.TMP_Text errorMessageText;
+    
+    [SerializeField] private TMPro.TMP_InputField passwordInput;
     public void Signin()
     {
+        if(string.IsNullOrEmpty(mailAddressInput.text) || string.IsNullOrEmpty(passwordInput.text))
+        {
+            errorMessageText.gameObject.SetActive(true);
+            return;
+        }
+        errorMessageText.gameObject.SetActive(false);
+        
         Debug.Log("Signin clicked");
     }
 }
